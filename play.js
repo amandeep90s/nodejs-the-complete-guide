8274c6c69267a87a496853732ae60ba0f5103b16
Amandeep Singh <programmeramandeep@gmail.com>
2023-07-08 20:44:22 +0530

    arrow functions

--- play.js
+++ play.js
@@ -3,7 +3,7 @@ let age = 33;
 const hasHobbies = true;
 age = 35;
 
-function summarizeUser(userName, userAge, userHasHobby) {
+const summarizeUser = (userName, userAge, userHasHobby) => {
   return (
     'Name is ' +
     userName +
@@ -12,6 +12,18 @@ function summarizeUser(userName, userAge, userHasHobby) {
     ' and the user has hobbies ' +
     userHasHobby
   );
-}
+};
 
 console.log(summarizeUser(name, age, hasHobbies));
+
+const addOne = (a) => a + 1;
+
+console.log(addOne(27));
+
+const sum = (a, b) => a + b;
+
+console.log(sum(4, 6));
+
+const addRandom = () => 27 + 28;
+
+console.log(addRandom());
