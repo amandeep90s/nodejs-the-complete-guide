8ace29809ef9951d7abf7b6e5af7ee1100c0b85a
Amandeep Singh <AmandeepSingh@emerson.com>
2023-07-11 10:00:23 +0530

    template strings

---
+++ template-string.js
@@ -0,0 +1,9 @@
+const fullname = "Amandeep Singh";
+const age = 33;
+const hobbies = ["Cricket", "Football", "Video Games", "Chess"];
+
+const str = `My name is ${fullname} and age is ${age}. My hobies are ${hobbies.join(
+  ", "
+)}.`;
+
+console.log(str);
