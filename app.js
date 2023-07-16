f045ddaba3ab3d10ea6f2176c9431d79cc356014
Amandeep Singh <programmeramandeep@gmail.com>
2023-07-16 11:23:30 +0530

    Blocking and non-blocking code

--- app.js
+++ app.js
@@ -23,14 +23,15 @@ const server = http.createServer((req, res) => {
       body.push(chunk);
     });
 
-    req.on('end', () => {
+    return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
     });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
   }
 
   res.write('<html>');
