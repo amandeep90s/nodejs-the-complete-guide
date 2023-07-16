5c040089b20d8d0fb1f9ccc56b2ee0d821474b0f
Amandeep Singh <programmeramandeep@gmail.com>
2023-07-16 08:26:09 +0530

    Sending responses

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log('Url', req.url);
-  console.log('Method', req.method);
-  console.log('Headers', req.headers);
-  process.exit();
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Custom-Header', 'test value');
+
+  res.write('<html>');
+  res.write('<head><title>My first nodejs page</title></head>');
+  res.write('<body><h1>Hello from Node.js Server</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000, () => {
