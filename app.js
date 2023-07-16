ab2afff8e81f73ee13bcaec523a4d4315e5e286b
Amandeep Singh <programmeramandeep@gmail.com>
2023-07-16 10:58:19 +0530

    Parsing request bodies

--- app.js
+++ app.js
@@ -17,7 +17,17 @@ const server = http.createServer((req, res) => {
   }
 
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
