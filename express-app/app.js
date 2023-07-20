cdcf839f1c47992d61c8192e82bf1be4c7917720
amandeep90s <programmeramandeep@gmail.com>
2023-07-20 22:44:04 +0530

    Parsing incoming requests

--- express-app/app.js
+++ express-app/app.js
@@ -2,6 +2,26 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json()); // for parsing application/json
+
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form method="POST" action="/product"><input type="text" name="product" /><input type="submit"  /></form>'
+  );
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  console.log('%s %s %s', req.method, req.url, req.path);
+  res.send('<h1>Hello from Express.js app</h1>');
+});
+
 app.listen(3000, () => {
   console.log(`Server running on localhost:3000`);
 });
