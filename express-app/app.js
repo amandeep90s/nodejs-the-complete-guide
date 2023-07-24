78d9c6fff25f617cff880c35ae1df568fde2afa2
amandeep90s <programmeramandeep@gmail.com>
2023-07-24 22:39:17 +0530

    Adding a 404 error route

--- express-app/app.js
+++ express-app/app.js
@@ -12,6 +12,10 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 app.use(shopRoutes);
 app.use(adminRoutes);
 
+app.use('*', (req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
+});
+
 app.listen(3000, () => {
   console.log(`Server running on localhost:3000`);
 });
