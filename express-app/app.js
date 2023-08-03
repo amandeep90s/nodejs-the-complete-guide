4e6f8c477cb7c5812ff0eaabe917a961315dd067
Amandeep Singh <programmeramandeep@gmail.com>
2023-08-03 19:04:23 +0530

    Converting html files to pug

--- express-app/app.js
+++ express-app/app.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
 
-const adminData = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", "views");
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 app.use(express.json()); // for parsing application/json
 
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.use(express.static(path.join(__dirname, "public"))); // for serving static files
+app.use(express.static(path.join(__dirname, 'public'))); // for serving static files
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 app.use(shopRoutes);
-app.use("/admin", adminData.router);
+app.use('/admin', adminData.router);
 
-app.use("*", (req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "errors", "404.html"));
+app.use('*', (req, res, next) => {
+  res.status(404).render('404', { docTitle: 'Page Not Found' });
 });
 
 app.listen(3000, () => {
--- express-app/routes/admin.js
+++ express-app/routes/admin.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
-const rootDir = require("../util/path");
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 const products = [];
 
-router.get("/add-product", (req, res, next) => {
-  res.status(200).sendFile(path.join(rootDir, "views", "add-product.html"));
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', { docTitle: 'Add Product' });
 });
 
-router.post("/add-product", (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   products.push({ title: req.body.title });
-  res.redirect("/");
+  res.redirect('/');
 });
 
 module.exports = { router, products };
